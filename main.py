64f6f9634336ba69d4c05e118e07e39c6b062338
m3gaf3di <amineblood3@gmail.com>
2024-08-08 15:09:24 +0100

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-main.py
+# main.py
+from visualization import plot_brainwaves
+
+if __name__ == "__main__":
+    muse = connect_to_muse()
+    if muse:
+        player = initialize_midi()
+        raw_data = start_stream(muse)
+        while True:
+            alpha, beta = preprocess_data(raw_data)
+            brainwaves_to_music(alpha, beta, player)
+            plot_brainwaves(alpha, beta)
