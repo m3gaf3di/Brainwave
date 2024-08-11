89dd4a6cd0e7b486f14bb610a9f912cdf56ca3d2
m3gaf3di <amineblood3@gmail.com>
2024-08-11 13:34:08 +0100

    eighth

--- front/brainwave/src/App.jsx
+++ front/brainwave/src/App.jsx
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>BRAINWAVE ON GOING </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 7)}>
           count is {count}
         </button>
         <p>
