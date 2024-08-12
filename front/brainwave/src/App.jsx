0c2da1245e6420555140b4c058ccd7b6eccb5252
m3gaf3di <amineblood3@gmail.com>
2024-08-12 13:29:46 +0100

    manipulating the vite front

--- front/brainwave/src/App.jsx
+++ front/brainwave/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './brainwavelogo.jpg'
 import './App.css'
 
 function App() {
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="./brainwavelogo.jpg" target="_blank">
+          <img src={viteLogo} className="logo" alt="brainwave logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
