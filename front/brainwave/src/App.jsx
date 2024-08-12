b2bfbaaae60f782dc3e2bbb3f1e62752c4059163
m3gaf3di <amineblood3@gmail.com>
2024-08-12 13:40:50 +0100

    added the logo and started implim

--- front/brainwave/src/App.jsx
+++ front/brainwave/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './brainwavelogo.jpg'
+import React from 'react';
+import brainwavelogo from './brainwavelogo.jpg';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="./brainwavelogo.jpg" target="_blank">
-          <img src={viteLogo} className="logo" alt="brainwave logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>BRAINWAVE ON GOING </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 7)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header className="App-header">
+      <img src={brainwavelogo} style={{ width: '100px', height: '100px' }} alt="Brainwave Logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to reload.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
